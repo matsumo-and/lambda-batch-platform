ff2c3b1c7093fb5124d937436473546a0e099efa
matsumo-and <yh134.toisanda@gmail.com>
2025-05-06 15:16:20 +0900

    fix/refactor_dir

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { BatchAStack } from "../lib/sample-stack";
+import { SampleStack } from "../lib/sample-stack";
 
 const app = new cdk.App();
-new BatchAStack(app, "BatchAStack", {
+new SampleStack(app, "SampleStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- cdk/lib/sample-stack.ts
+++ cdk/lib/sample-stack.ts
@@ -4,25 +4,26 @@ import {
   aws_lambda as lambda,
   aws_events as events,
   aws_events_targets as targets,
+  Duration,
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import path from "path";
 
-export class BatchAStack extends Stack {
+export class SampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const fn = new lambda.Function(this, "BatchAFunction", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(
-        path.resolve(__dirname, "../../batches/batch-a/dist"),
+    const lambdaFunc = new lambda.DockerImageFunction(this, "SampleFunction", {
+      code: lambda.DockerImageCode.fromImageAsset(
+        path.resolve(__dirname, "../../lambda/sample-project"),
       ),
+      memorySize: 256,
+      timeout: Duration.seconds(10),
     });
 
-    new events.Rule(this, "ScheduleRule", {
+    new events.Rule(this, "SampleFunctionRule", {
       schedule: events.Schedule.cron({ minute: "0", hour: "0" }), // 毎日0時
-      targets: [new targets.LambdaFunction(fn)],
+      targets: [new targets.LambdaFunction(lambdaFunc)],
     });
   }
 }
